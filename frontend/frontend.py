6af302c6c4642802586e317ee23d5418e4f7641d
nick-casa <nc16@princeton.edu>
2024-05-04 13:31:34 -0400

    test

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -36,4 +36,4 @@ if __name__ == "__main__":
     # https://stackoverflow.com/questions/17260338/deploying-flask-with-heroku
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(port=port, debug=True)
