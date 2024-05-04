51bd17b8462e5ad9c93ff84fa3d38106bc4a7991
nick-casa <nc16@princeton.edu>
2024-05-04 12:20:44 -0400

    fixes

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 import requests  # Import the requests library
+import os
+
 
 app = Flask(__name__)
 
@@ -31,6 +33,8 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(
-        debug=True, port=5001
-    )  # Ensure this is running on a different port than the backend
+    # below arrangment is done specially for Heroku
+    # https://stackoverflow.com/questions/17260338/deploying-flask-with-heroku
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
