5cfeea847ef90544382da26eab07836c519bb2eb
nick-casa <nc16@princeton.edu>
2024-05-04 12:06:36 -0400

    fixes

--- app.py
+++ app.py
@@ -31,6 +31,5 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(
-        debug=True, port=5001
-    )  # Ensure this is running on a different port than the backend
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port)
